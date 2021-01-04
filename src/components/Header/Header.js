912c587cd2a0c6dda3346ce3a55205ee63655b54
Ben Freudman <bfreud94@gmail.com>
2021-01-03 22:27:03 -0500

    Minor formatting change for the Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,9 +21,9 @@ class Header extends Component {
                         My Shopping List
                         <br />
                         <React.Fragment>
-                            <Link style={linkStyle} to="/addItem">Create Item | </Link>
-                            <Link style={linkStyle} to="/itemsList">See All Items | </Link>
-                            <Link style={linkStyle} to="/login" onClick={this.logout}>Logout</Link>
+                            <Link style={linkStyle} to='/addItem'>Create Item | </Link>
+                            <Link style={linkStyle} to='/itemsList'>See All Items | </Link>
+                            <Link style={linkStyle} to='/login' onClick={this.logout}>Logout</Link>
                         </React.Fragment>
                     </header>
                 )
